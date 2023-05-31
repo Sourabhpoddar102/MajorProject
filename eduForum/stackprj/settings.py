0f2109e425380e2d198cd7aa4d9330a51682a588
Sourabh <sourabhpoddar102@gmail.com>
2023-05-31 22:32:36 +0530

    next commit

--- eduForum/stackprj/settings.py
+++ eduForum/stackprj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-jaelsf1%p)ryf^8w7-&o9e+4rqpiyn9*j_!3r1q#t@&+b!8+bh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 
